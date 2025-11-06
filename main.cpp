8696d84e1af17434c3972778ec865c0c33d0d2da
Jacobo0991 <00005822@uca.edu.sv>
2025-11-06 01:32:11 -0600

    add: algoritmos primer y  mejor ajuste y ejemplo de uso

--- main.cpp
+++ main.cpp
@@ -25,5 +25,23 @@ int main() {
     mem.reset();
     mem.holes().printFreeList();
 
+    // Ejemplo de uso algoritmos 
+    std::cout << "\n=== Prueba de primer ajuste ===\n";
+    mem.holes().clear();
+    mem.holes().pushHoleSortedAndCoalesce(0, 30);
+    mem.holes().pushHoleSortedAndCoalesce(70, 20);
+    mem.holes().printFreeList();
+
+    // Busca el primer ajuste para un espacio de 10 bytes
+    FreeBlock* prevFirstFind = nullptr;
+    FreeBlock* firstFind = mem.holes().findFirstFit(10, prevFirstFind);
+    std::cout << "\n Hueco encontrado en "<< firstFind->start << ". Con tamano " << firstFind->size <<"\n";
+    
+    std::cout << "\n=== Prueba de mejor ajuste ===\n";
+    // Busca el mejor ajuste para un espacio de 10 bytes
+    FreeBlock* prevBestFind = nullptr;
+    FreeBlock* bestFind = mem.holes().findBestFit(10, prevBestFind);
+    std::cout << "\n Hueco encontrado en "<< bestFind->start << ". Con tamano " << bestFind->size <<"\n";
+
     return 0;
 }
